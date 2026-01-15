5d985d51351b20e7beea1340dc78eaedf3aa152f
Muhammad Ridho Febriyansyah <mhmmadridho64@gmail.com>
2026-01-15 15:26:07 +0700

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -687,8 +687,8 @@ const App = () => {
 
     try {
       const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY_SPESIAL}`, 
-  {
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY_SPESIAL}`,
+        {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
